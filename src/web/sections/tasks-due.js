0a20aef78f19a23475ebdf3de2f38a97f078e37a
Thomas Schoffelen <thomas@includable.com>
2019-04-21 15:31:12 +0200

    Cleanup of due dates graph.

--- src/web/sections/tasks-due.js
+++ src/web/sections/tasks-due.js
@@ -5,8 +5,8 @@ module.exports = {
   render: async () => {
     let rows = await db.query('SELECT start, startDate FROM TMTask WHERE type = 0 AND status = 0')
     let months = {
-      'Overdue': 0,
       'Today': 0,
+      'Tomorrow': 0,
       'This week': 0,
       'Next week': 0,
       'Later': 0,
@@ -14,21 +14,23 @@ module.exports = {
       'Someday': 0
     }
 
-    const overdue = moment().startOf('day').valueOf()
+    let colors = ['#f9d348', '#a38731', '#65aca4', '#50817c', '#395755', '#7dabf6', '#d1c794']
+
     const today = moment().endOf('day').valueOf()
-    const thisWeek = moment().startOf('isoWeek').add('7', 'days').valueOf()
-    const nextWeek = moment().startOf('isoWeek').add('14', 'days').valueOf()
+    const tomorrow = moment().endOf('day').add(1, 'days').valueOf()
+    const thisWeek = moment().startOf('isoWeek').add(7, 'days').valueOf()
+    const nextWeek = moment().startOf('isoWeek').add(14, 'days').valueOf()
 
     rows.forEach(row => {
-      if(!row.startDate){
+      if (!row.startDate) {
         months[row.start === 1 ? 'No date' : 'Someday']++
         return
       }
       const value = Math.round(parseFloat(row.startDate)) * 1000
-      if (value < overdue) {
-        months['Overdue']++
-      } else if (value < today) {
+      if (value < today) {
         months['Today']++
+      } else if (value < tomorrow) {
+        months['Tomorrow']++
       } else if (value < thisWeek) {
         months['This week']++
       } else if (value < nextWeek) {
@@ -38,13 +40,19 @@ module.exports = {
       }
     })
 
+    if (thisWeek - 1 === today) {
+      delete months['This week']
+      delete colors[4]
+      colors = Object.values(colors)
+    }
+
     return {
       type: 'bar',
       data: {
         labels: Object.keys(months),
         datasets: [{
           data: Object.values(months),
-          backgroundColor: ['#eb476e', '#f9d348', '#65aca4', '#50817c', '#395755', '#7dabf6', '#d1c794']
+          backgroundColor: colors
         }]
       },
       options: {
